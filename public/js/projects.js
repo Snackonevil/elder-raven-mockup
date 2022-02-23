a07ab013248de7c438da43d121c99d7dfdaa73a1
Kevin Lacson <lacsonky@gmail.com>
2022-02-22 23:05:49 -0500

    toggle project states

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,19 +1,26 @@
 const projects = Array.from(document.querySelectorAll(".project"));
 const projectSection = document.querySelector(".section-3");
 
+const showButtonContainer = project => {
+    const btnContainer =
+        project.children[0].children[project.children[0].children.length - 1];
+    btnContainer.classList.remove("hidden");
+};
+
 const openProjectState = project => {
     const overlay = project.children[1];
     project.classList.add("active-project");
     project.classList.remove("inactive-project");
     overlay.style.height = "25%";
     overlay.style.opacity = "1";
-    const btnContainer =
-        project.children[0].children[project.children[0].children.length - 1];
-    btnContainer.classList.remove("hidden");
+    overlay.children[0].style.width = "50%";
+    overlay.children[1].classList.remove("hidden");
+    console.log(overlay.children);
+    showButtonContainer(project);
 };
 
-const closeProjectState = () => {
-    // reset ALL project states
+// Reset ALL project states
+const resetAllProjectStates = () => {
     projects.forEach(project => {
         const overlay = project.children[1];
         const btnContainer =
@@ -25,20 +32,15 @@ const closeProjectState = () => {
         overlay.style.height = "";
         overlay.style.opacity = "";
         btnContainer.classList.add("hidden");
+        overlay.children[0].style.width = "";
+        overlay.children[1].classList.add("hidden");
     });
 };
 
+// Event listener to open project states
 projects.forEach(project => {
     project.addEventListener("click", e => {
-        closeProjectState();
+        resetAllProjectStates();
         openProjectState(project);
-
-        const projectCarousel = project.children[0];
-        const overlay = project.children[1];
-        const btnContainer =
-            project.children[0].children[
-                project.children[0].children.length - 1
-            ];
-        console.log(btnContainer);
     });
 });
