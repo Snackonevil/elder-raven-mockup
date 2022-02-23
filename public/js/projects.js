5693c91e939029b733e4d2da345ae2a1b4df8e0c
Kevin Lacson <lacsonky@gmail.com>
2022-02-22 22:52:08 -0500

    selecting project card elements with js

--- public/js/projects.js
+++ public/js/projects.js
@@ -7,15 +7,24 @@ const openProjectState = project => {
     project.classList.remove("inactive-project");
     overlay.style.height = "25%";
     overlay.style.opacity = "1";
+    const btnContainer =
+        project.children[0].children[project.children[0].children.length - 1];
+    btnContainer.classList.remove("hidden");
 };
 
 const closeProjectState = () => {
+    // reset ALL project states
     projects.forEach(project => {
         const overlay = project.children[1];
+        const btnContainer =
+            project.children[0].children[
+                project.children[0].children.length - 1
+            ];
         project.classList.remove("active-project");
         project.classList.add("inactive-project");
         overlay.style.height = "";
         overlay.style.opacity = "";
+        btnContainer.classList.add("hidden");
     });
 };
 
@@ -23,9 +32,12 @@ projects.forEach(project => {
     project.addEventListener("click", e => {
         closeProjectState();
         openProjectState(project);
+
+        const projectCarousel = project.children[0];
+        const overlay = project.children[1];
         const btnContainer =
-            e.currentTarget.children[0].children[
-                e.currentTarget.children[0].children.length - 1
+            project.children[0].children[
+                project.children[0].children.length - 1
             ];
         console.log(btnContainer);
     });
