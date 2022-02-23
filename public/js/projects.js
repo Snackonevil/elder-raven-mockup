199d8b20c811113a83330e830bc59d6953e21d24
Kevin Lacson <lacsonky@gmail.com>
2022-02-22 23:31:11 -0500

    refactor html and project.js

--- public/js/projects.js
+++ public/js/projects.js
@@ -7,15 +7,20 @@ const showButtonContainer = project => {
     btnContainer.classList.remove("hidden");
 };
 
+const restyleOverlay = overlay => {
+    // Restyle overlay
+    overlay.style.height = "25%"; // Height of overlay
+    overlay.style.opacity = "1"; // Opacity of overlay
+    overlay.children[0].style.width = "50%"; // Width of Project name
+    overlay.children[1].classList.remove("hidden"); // Reveal description of project
+};
+
 const openProjectState = project => {
     const overlay = project.children[1];
+    // Enlarge project card
     project.classList.add("active-project");
     project.classList.remove("inactive-project");
-    overlay.style.height = "25%";
-    overlay.style.opacity = "1";
-    overlay.children[0].style.width = "50%";
-    overlay.children[1].classList.remove("hidden");
-    console.log(overlay.children);
+    restyleOverlay(overlay);
     showButtonContainer(project);
 };
 
@@ -29,11 +34,11 @@ const resetAllProjectStates = () => {
             ];
         project.classList.remove("active-project");
         project.classList.add("inactive-project");
-        overlay.style.height = "";
-        overlay.style.opacity = "";
-        btnContainer.classList.add("hidden");
-        overlay.children[0].style.width = "";
-        overlay.children[1].classList.add("hidden");
+        overlay.style.height = ""; // Reset overlay state
+        overlay.style.opacity = ""; // Reset overlay state
+        btnContainer.classList.add("hidden"); // Hide button container
+        overlay.children[0].style.width = ""; // Restyle project name
+        overlay.children[1].classList.add("hidden"); // Hide project description
     });
 };
 
@@ -43,4 +48,24 @@ projects.forEach(project => {
         resetAllProjectStates();
         openProjectState(project);
     });
+});
+
+const leftBtns = Array.from(document.querySelectorAll(".project-left"));
+const rightBtns = Array.from(document.querySelectorAll(".project-right"));
+
+const projectCarousels = Array.from(
+    document.querySelectorAll(".project-carousel")
+);
+
+const projectSlides = Array.from(projectCarousels[0].children);
+
+console.log(projectSlides);
+
+leftBtns.forEach(leftBtn => {
+    leftBtn.addEventListener("click", () =>
+        console.log(leftBtn.parentElement.parentElement)
+    );
+});
+rightBtns.forEach(rightBtn => {
+    rightBtn.addEventListener("click", () => console.log("click right"));
 });
